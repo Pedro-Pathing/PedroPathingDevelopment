e8af32f185fe4ffdd77be1a69fbcca8976d50758
Rishi Ponnapalli <rishiponnapalli13@gmail.com>
2025-03-21 13:47:47 -0400

    Added comments

--- src/main/java/com/pedropathing/follower/FollowerVectors.java
+++ src/main/java/com/pedropathing/follower/FollowerVectors.java
@@ -2,12 +2,25 @@ package com.pedropathing.follower;
 
 import com.pedropathing.pathgen.Vector;
 
+/**
+ * This class holds the vectors used for following a path.
+ *
+ * @author Rishi Ponnapalli - 19411 Tech Tigers
+ */
 public class FollowerVectors {
     public final Vector correctiveVector;
     public final Vector headingVector;
     public final Vector pathingVector;
     public final double robotHeading;
 
+    /**
+     * This creates a new FollowerVectors object.
+     *
+     * @param correctiveVector the corrective vector
+     * @param headingVector the heading vector
+     * @param pathingVector the pathing vector
+     * @param robotHeading the robot's heading
+     */
     public FollowerVectors(Vector correctiveVector, Vector headingVector,
                            Vector pathingVector, double robotHeading) {
         this.correctiveVector = correctiveVector;
